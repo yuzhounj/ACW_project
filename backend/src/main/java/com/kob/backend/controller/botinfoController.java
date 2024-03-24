d94f4bb2651960b36f7aff3223388caa88ed4c14
zhouyu <2205395916@qq.com>
2024-03-24 15:04:50 +0800

    第一次实现

--- backend/src/main/java/com/kob/backend/controller/botinfoController.java
+++ backend/src/main/java/com/kob/backend/controller/botinfoController.java
@@ -3,11 +3,17 @@ package com.kob.backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class botinfoController {
     @RequestMapping("/getbot")
-    public String getbot(){
-        return "hhh";
+    public Map<String,String> getbot(){
+          Map<String,String>bot1=new HashMap<>();
+          bot1.put("name","tiger");
+          bot1.put("rating","1500");
+          return bot1;
     }
 }
